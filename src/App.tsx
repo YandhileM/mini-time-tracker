1bbadd96aef2fad1100b5de22f10dcd7eb7c2de2
Yandhile <yandhilemaluleke@gmail.com>
2025-06-21 13:07:58 +0200

    add TotalHours component to display total tracked hours in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./styles.css";
 import { TimeEntry } from "./types/types";
 import TimeEntryForm from "./components/TimeEntryForm";
+import TotalHours from './components/TotalHours';
 
 function App() {
   const [entries, setEntries] = useState<TimeEntry[]>([]);
@@ -9,11 +10,13 @@ function App() {
   const addEntry = (taskName: string, hours: number) => {
     setEntries([...entries, { id: Date.now().toString(), taskName, hours }]);
     console.log(`Added entry: ${taskName} for ${hours} hours`);
-  };
+    };
+    
   return (
     <div className="app">
       <h1>Time Tracker</h1>
-      <TimeEntryForm onAdd={addEntry} />
+          <TimeEntryForm onAdd={addEntry} />
+          <TotalHours entries={entries} />
     </div>
   );
 }
